e54013b64cfe63d56b4a3c125d86dbcc01c265c5
Diogo <diogpcam@gmail.com>
2024-09-14 10:57:08 -0300

    Ajustes realizados e limpeza de colunas desnecessárias.

--- services/CandidateService.py
+++ services/CandidateService.py
@@ -4,13 +4,14 @@ import pandas as pd
 class CandidateService:
     def __init__(self):
         csv_path = os.path.join(os.path.dirname(__file__), '..', 'repository', 'candidatos_poa_rs_2024.csv')
-        self.df = pd.read_csv(csv_path)
+        excel_path = os.path.join(os.path.dirname(__file__), '..', 'repository', 'candidatos_poa_rs_2024.xlsx')
+        self.df = pd.read_excel(excel_path)
 
     def obter_dados(self):
         return self.df
 
     def get_from_party(self, party_name):
-        candidatos_do_partido = self.df[self.df.iloc[:, 26] == party_name]
+        candidatos_do_partido = self.df[self.df.iloc[:, 8] == party_name]
         lista_candidatos = candidatos_do_partido.to_dict(orient='records')
         return lista_candidatos
 
